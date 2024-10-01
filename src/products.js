6f78f73dd53ca1a6917b39ecdf734358bef58787
JohariRitik <77263490+JohariRitik@users.noreply.github.com>
2024-10-01 19:46:09 +0000

    Pending changes exported from your codespace

--- src/products.js
+++ src/products.js
@@ -11,31 +11,31 @@ export const PRODUCTS = [
   {
     id: 1,
     productName: "IPhone",
-    price: 999.0,
+    price: 70000,
     productImage: product1,
   },
   {
     id: 2,
     productName: "Macbook Pro 2022 (M1)",
-    price: 1999.0,
+    price: 61999,
     productImage: product2,
   },
   {
     id: 3,
     productName: "Cannon M50 Camera",
-    price: 699.0,
+    price: 39999,
     productImage: product3,
   },
   {
     id: 4,
     productName: "WLS Van Gogh Denim Jacket",
-    price: 228.0,
+    price: 3999,
     productImage: product4,
   },
   {
     id: 5,
     productName: "LED Light Strips",
-    price: 19.99,
+    price: 899,
     productImage: product5,
   },
   {
